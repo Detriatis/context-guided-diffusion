744b6886053b0ec87497b7111d220643f109c07e
mdodds665 <mdodds665@gmail.com>
2025-06-20 19:14:17 +0100

    updated utils

--- swiss_roll/src/swiss_roll/utils.py
+++ swiss_roll/src/swiss_roll/utils.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn 
 import yaml
+import numpy as np 
+from swiss_roll import DATA_DIR
 
 def save_model(model: nn.Module, path):
     with open(path, 'wb') as f:
@@ -21,3 +23,14 @@ def load_metrics(path):
     with open(path, 'r') as f:
         metrics = yaml.load(f, yaml.FullLoader)
     return metrics
+
+def load_swissroll(path=None):
+    if not path: 
+        path = DATA_DIR / 'swiss_roll.npz'
+       
+    with open(path, 'rb') as f: 
+        data = np.load(f)
+        xyz_points = data['xyz_points']
+        manifold_points = data['manifold_points']
+     
+    return torch.tensor(xyz_points, dtype=torch.float32), torch.tensor(manifold_points, dtype=torch.float32).view(-1, 1)
