edc290bcafab73060b8e38ecf0833f0f1b941b99
Michael Dodds <e56218md@login3.csf3.man.alces.network>
2025-06-27 10:45:52 +0100

    hanges

--- swiss_roll/src/opt_guidance.sh
+++ swiss_roll/src/opt_guidance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=model_eval
-#SBATCH --array=0-1
+#SBATCH --array=2-52
 #SBATCH --partition=multicore
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=2G
@@ -11,14 +11,16 @@ module purge
 source "$(conda info --base)"/etc/profile.d/conda.sh
 conda activate swiss_roll
 
-# SLURM_ARRAY_TASK_ID=1
-BATCH_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
 # SCRIPTDIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
+# SLURM_ARRAY_TASK_ID=1
+
+BATCH_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
 SCRIPTDIR="$SLURM_SUBMIT_DIR"
 TMPDIR=${SLURM_TMPDIR:-/tmp}
 BATCHCONFIGS=${SCRIPTDIR}/../conf/batched_configs
-
 BATCH_TARBALL=${BATCHCONFIGS}/batch_${BATCH_ID}.tar.gz
+
+echo $TMPDIR
 
 cp $BATCH_TARBALL $TMPDIR
 mkdir -p $TMPDIR/batch_$BATCH_ID/conf
@@ -26,8 +28,8 @@ tar -xzf $TMPDIR/batch_${BATCH_ID}.tar.gz -C $TMPDIR/batch_$BATCH_ID/conf
 
 for CONF in ${TMPDIR}/batch_${BATCH_ID}/conf/*.yaml; do
     RUN_ID=$(basename "$CONF" .yaml)
-    WRITEOUT="$TMPDIR/batch_$BATCH_ID/output/run_${RUN_ID}"
-    LOGGER="$TMPDIR/batch_$BATCH_ID/output/run_${RUN_ID}/logger"
+    WRITEOUT="$TMPDIR/batch_$BATCH_ID/run_${RUN_ID}"
+    LOGGER="${SCRIPTDIR}/../runs/guidance_models/batch_$BATCH_ID/run_${RUN_ID}/logger"
     
     mkdir -p "$WRITEOUT"
 
@@ -38,7 +40,7 @@ for CONF in ${TMPDIR}/batch_${BATCH_ID}/conf/*.yaml; do
         --logger "$LOGGER"
 done
 
-cp -r $TMPDIR/batch_$BATCH_ID/output ${SCRIPTDIR}/../runs/guidance_models/batch_${BATCH_ID}
+cp -r $TMPDIR/batch_$BATCH_ID/* ${SCRIPTDIR}/../runs/guidance_models/batch_${BATCH_ID}
 
 rm -r ${TMPDIR}/batch_${BATCH_ID}
 rm -r ${TMPDIR}/batch_${BATCH_ID}.tar.gz
